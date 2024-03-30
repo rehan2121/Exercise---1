33879a80129b040b9681eff3a3a9d653d07d1aa8
Rehan Pathan <133865530+rehan2121@users.noreply.github.com>
2024-03-30 23:11:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# Making The Calculator In Python
+# Author : REHAN PATHAN
+
+a = 9
+b = 9
+
+print(a," + ", b, " = ", a + b)
+# Performing (Addition)
+
+print(a," - ", b, " = ",  a - b)
+# Performing (Subtraction)
+
+print(a," * ", b, " = ",  a * b)
+# Performing (Multiplication)
+
+print(a," / ", b, " = ",  a / b)
+# Performing (Division)
+
+print(a," ** ", b, " = ",  a ** b)
+# Performing (Expontiation)
+
+print(a," // ", b, " = ",  a // b)
+# Performing (Floor-Division)
+
+print(a," % ", b, " = ",  a % b)
+# Performing (Modulus)
